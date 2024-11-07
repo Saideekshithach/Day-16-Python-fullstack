7ad6388651389fa81027330194475c15de169305
Saideekshitha <104785671+Saideekshithach@users.noreply.github.com>
2024-11-07 21:18:19 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,90 @@
+#range
+# range function
+#start-stop--step
+'''for i in range(5):
+    print(i)'''
+
+'''for i in range(7,15):
+    print(i)'''
+
+'''for i in range(5):
+    print(i,end=" ")
+    
+for i in range(24,45):
+    print(i)'''
+
+#task
+'''s=int(input("enter number:"))
+e=int(input("enter number:"))
+count=int(input("enter number:"))
+for i in range(s,e,count):
+    print(i,end=" ")'''
+
+'''for i in range(5,50,5):
+    print(i,end=" ")'''
+6
+'''for i in range(3,30,3):
+    print(i,end=" ")'''
+
+#grade code
+'''while True:
+    marks=int(input("enter marks:"))
+    if marks in range(91,101):
+        print("A grade")
+    elif marks in range(81,91):
+        print("B grade")
+    elif marks in range(71,81):
+        print("C grade")
+    elif marks in range(50,71):
+        print("D grade")
+    else:
+        print("FAIL")'''
+
+'''marks=int(input("enter the marks:"))
+while marks in range(91,101):
+    print("Grade-A")'''
+
+'''for i in range(5):
+    i=i+1
+    print(i)
+
+for i in range(5):
+    i=i+2
+    print(i)'''
+#break,continue and pass statements
+#break
+'''a=10
+while a>1:
+    print(a)
+    a=a-1
+    if a==5:
+        break'''
+#continue
+'''a=20
+while a>1:
+    print(a)
+    a=a-1
+    if a==6:
+        continue'''
+'''a=20
+while a>1:
+    a=a-1
+    if a==6:
+        continue
+    print(a)'''
+
+
+
+
+a=5
+for i in range(a):
+    if i%2==0:
+        continue
+    print(i)
+
+    
+
+
+
+
+ 
